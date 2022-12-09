17f2c1c25eb305223c8c7e114923afce2e76eee8
6hiro <hiro6github@gmail.com>
2022-12-09 12:16:07 +0900

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <head prefix="og: https://ogp.me/ns#" >
-        <meta property="og:url" content="" >
-        <meta property="og:type" content="website" >
+
         <meta property="og:title" content="Vlides">
-        <meta property="og:description" content="Slides With Voice" >
-        <meta property="og:site_name" content="Vlides" />
-        <meta property="og:image" content="https://vlides.com" />
-        <meta name="twitter:card" content="summary" /> 
-        <meta name="twitter:site" content="@Vlides_" />
+        <meta property="og:description" content="Slides With Voice">
+        <meta property="og:image" content="https://vlides.com/images/LogoOGP.png">
+        <meta property="og:image:secure_url" content="https://vlides.com/images/LogoOGP.png">
+        <meta property="og:url" content="https://vlides.com">
+        <meta property="og:site_name" content="Vlides">
+        <meta property="og:image:width" content="300">
+        <meta property="og:image:height" content="300">
+        <meta name="twitter:title" content="Vlides">
+        <meta name="twitter:description" content="Slides With Voice">
+        <meta name="twitter:image" content="https://vlides.com/images/LogoOGP.png">
+        <meta name="twitter:card" content="summary">
+        {{-- <meta name="twitter:site" content="@Vlides_"> --}}
 
         <title>Vlides</title>
 
