695193952f7d0118bab8d1ba3c86880185dc8c66
6hiro <hiro6github@gmail.com>
2022-12-09 11:57:05 +0900

    first commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <head prefix="og: https://ogp.me/ns#" >
+        <meta property="og:url" content="" >
+        <meta property="og:type" content="website" >
+        <meta property="og:title" content="Vlides">
+        <meta property="og:description" content="Slides With Voice" >
+        <meta property="og:site_name" content="Vlides" />
+        <meta property="og:image" content="https://vlides.com" />
+        <meta name="twitter:card" content="summary" /> 
+        <meta name="twitter:site" content="@Vlides_" />
+
         <title>Vlides</title>
 
         {{-- react に変更があったとき自動で --}}
