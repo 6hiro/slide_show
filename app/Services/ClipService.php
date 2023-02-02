41f9145ed9945cb2a831603c024b9446ebacb1a0
6hiro <hiro6github@gmail.com>
2023-02-03 00:18:11 +0900

    initial commit

--- app/Services/ClipService.php
+++ app/Services/ClipService.php
@@ -106,8 +106,8 @@ class ClipService
         if($since){
             $clips = Clip::with(['tags', 'user', 'replies', 'replies.user'])
                 ->whereHas('tags', function($query) use ($tag_name)  {
-                    $query->where('name', $tag_name);
-                    // $query->where('alias', strtolower($tag_name));
+                    // $query->where('name', $tag_name);
+                    $query->where('alias', strtolower($tag_name));
                 })
                 ->where('created_at', '<', $since)
                 ->latest('created_at')
@@ -116,8 +116,8 @@ class ClipService
         }else{
             $clips = Clip::with(['tags', 'user', 'replies', 'replies.user'])
                 ->whereHas('tags', function($query) use ($tag_name)  {
-                    $query->where('name', $tag_name);
-                    // $query->where('alias', strtolower($tag_name));
+                    // $query->where('name', $tag_name);
+                    $query->where('alias', strtolower($tag_name));
                 })
                 ->latest('created_at')
                 ->take($per_page+1)
@@ -132,8 +132,8 @@ class ClipService
         $clips = Clip::with(['tags', 'user', 'replies', 'replies.user'])
             ->where('created_at', '>', $since)
             ->whereHas('tags', function($query) use ($tag_name)  {
-                $query->where('name', $tag_name);
-                // $query->where('alias', strtolower($tag_name));
+                // $query->where('name', $tag_name);
+                $query->where('alias', strtolower($tag_name));
             })
             ->withCount('likes')
             ->orderBy('likes_count', 'desc')
@@ -318,7 +318,8 @@ class ClipService
                 if(  preg_match( $pattern, $sentence ) ){
                     $tag = Tag::where('name', substr($sentence, 1))->first();
 
-                    if (empty($tag)) {
+                    // if (empty($tag)) {
+                    if (empty($tag) || $tag->name !== substr($sentence, 1)) {                    
                         $tag = Tag::create([
                             'name' => substr($sentence, 1),
                             'alias' => strtolower(substr($sentence, 1)),
@@ -449,7 +450,8 @@ class ClipService
                 if(  preg_match( $pattern, $sentence ) ){
                     $tag = Tag::where('name', substr($sentence, 1))->first();
 
-                    if (empty($tag)) {
+                    // if (empty($tag)) {
+                    if (empty($tag) || $tag->name !== substr($sentence, 1)) {
                         $tag = Tag::create([
                             'name' => substr($sentence, 1),
                             'alias' => strtolower(substr($sentence, 1)),
