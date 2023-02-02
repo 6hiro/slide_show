8a345b835b0e76409bdd57c7cdff261679f720a3
6hiro <hiro6github@gmail.com>
2023-02-03 00:41:10 +0900

    initial commit

--- app/Services/ClipService.php
+++ app/Services/ClipService.php
@@ -325,10 +325,10 @@ class ClipService
                             'alias' => strtolower(substr($sentence, 1)),
                         ]);
                     }else if($tag && $tag->name !== substr($sentence, 1)) {
-                        $tag = Tag::create([
-                            'name' => substr($sentence, 1),
-                            'alias' => strtolower(substr($sentence, 1)),
-                        ]);
+                        // $tag = Tag::create([
+                        //     'name' => substr($sentence, 1),
+                        //     'alias' => strtolower(substr($sentence, 1)),
+                        // ]);
                     }
                     $tag_id_list[] = $tag->id;
                 }
